af8605ecdb469ffd873e526796eadf069ebb4eca
Daniel Liang <mr.daniel.liang@gmail.com>
2023-04-18 15:34:07 -0400

    feat: add gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/* 
+  Example GraphQL query for siteMetdata
+
+  query GetSiteMetadata {
+    site {
+      siteMetadata {
+        siteUrl
+        title
+        description
+        image
+      }
+    }
+  }
+*/
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Frontend Masters Intro to Gatsby',
+    description: 'Frontend Masters Intro to Gatsby course project',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+};
